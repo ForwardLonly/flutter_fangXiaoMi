a83f5754aff5089ad725e3111de41bf11547b670
zhuzhu <15210134184@163.com>
2025-09-22 22:01:10 +0800

    [common] 商品详情页的更多点击弹出菜单 + 底部悬浮tabbar： 1.添加一条边框：border:Border(             top: BorderSide(width: Screenadapter.width(2), color: Colors.black12),           ) 2.按钮添加一部分的圆角：style: ButtonStyle(                   padding: WidgetStateProperty.all(EdgeInsets.zero),                   backgroundColor: WidgetStatePropertyAll(Colors.orange),                   foregroundColor: WidgetStatePropertyAll(Colors.white),                   // 按钮设置圆角                   shape: WidgetStateProperty.all(                     RoundedRectangleBorder(                       borderRadius: BorderRadius.only(                         topLeft: Radius.circular(Screenadapter.width(55)),                         bottomLeft: Radius.circular(Screenadapter.width(55)),                       )                     )                   )                 ) [version] 0.1.0

--- flutter_fangxm/lib/app/modules/product_content/views/product_content_view.dart
+++ flutter_fangxm/lib/app/modules/product_content/views/product_content_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fangxm/app/service/ScreenAdapter.dart';
+import 'package:flutter_fangxm/app/sources/FangXMIcon.dart';
 
 import 'package:get/get.dart';
 
@@ -14,14 +15,14 @@ class ProductContentView extends GetView<ProductContentController> {
       extendBodyBehindAppBar: true, 
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(Screenadapter.height(140)),
-        child: _appBarWidget()
+        child: _appBarWidget(context)
       ),
       body: _bodyWidget(),
     );
   }
 
   // 自定义导航栏
-  Widget _appBarWidget() {
+  Widget _appBarWidget(BuildContext context) {
     return Obx(()=> AppBar(
       leading: _appBarLeadingWidget(),
       title: _appBarTitleWidget(),
@@ -30,7 +31,7 @@ class ProductContentView extends GetView<ProductContentController> {
       backgroundColor: Colors.white.withAlpha(controller.opacity.value),
       // 实现透明导航 第三步
       elevation: 0,
-      actions: _appBarAtionsWidget(),
+      actions: _appBarAtionsWidget(context),
     ));
   }
   // 自定义 返回按钮
@@ -112,7 +113,7 @@ class ProductContentView extends GetView<ProductContentController> {
   }
 
   // 自定义右边操作视图
-  List<Widget> _appBarAtionsWidget() {
+  List<Widget> _appBarAtionsWidget(BuildContext context) {
     return [
       Container(
         margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
@@ -138,7 +139,9 @@ class ProductContentView extends GetView<ProductContentController> {
             backgroundColor: WidgetStateProperty.all(Colors.black12),
             shape: WidgetStateProperty.all(CircleBorder())
           ),
-          onPressed: (){}, 
+          onPressed: (){
+            _showMoreButtonMenu(context);
+          }, 
           child: Icon(Icons.more_horiz_rounded, color: Colors.white)
         )
       ),
@@ -147,6 +150,15 @@ class ProductContentView extends GetView<ProductContentController> {
 
   // 自定义内容视图
   Widget _bodyWidget() {
+    return Stack(
+      children: [
+        _bodyContentWidget(),
+        _bottomTabbarWidget()
+      ],
+    );
+  }
+
+  Widget _bodyContentWidget() {
     // tab点击跳转到对应的锚点：第二步， 用SingleChildScrollView代替ListView
     return SingleChildScrollView(
       controller: controller.scrollController,
@@ -176,8 +188,151 @@ class ProductContentView extends GetView<ProductContentController> {
             height: Screenadapter.height(3800),
             child: Text("推荐", style: TextStyle(fontSize: 100)),
           ),
+          _bottomTabbarWidget()
         ],
       ),
+    );
+  }
+
+  // 显示弹窗
+  void _showMoreButtonMenu(BuildContext context) {
+    showMenu(
+      color: Colors.black38,
+      context: context, 
+      position: RelativeRect.fromLTRB(
+        Screenadapter.width(800), 
+        Screenadapter.height(300), 
+        Screenadapter.width(20), 
+        0
+      ), 
+      items: [
+        PopupMenuItem(child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.home, color: Colors.white),
+            SizedBox(width: 10),
+            Text("首页", style: TextStyle(color: Colors.white))
+          ],
+        )),
+        PopupMenuItem(child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.message, color: Colors.white),
+            SizedBox(width: 10),
+            Text("消息", style: TextStyle(color: Colors.white))
+          ],
+        )),
+        PopupMenuItem(child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.star, color: Colors.white),
+            SizedBox(width: 10),
+            Text("收藏", style: TextStyle(color: Colors.white))
+          ],
+        )),
+      ]
+    );
+  }
+
+  // 底部浮动导航栏
+  Widget _bottomTabbarWidget() {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Container(
+        width: double.infinity,
+        height: Screenadapter.width(190),
+        padding: EdgeInsets.fromLTRB(0, Screenadapter.width(20), Screenadapter.width(40), 0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          // 只设置一条边框
+          border: Border(
+            top: BorderSide(width: Screenadapter.width(2), color: Colors.black12),
+          )
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: Screenadapter.width(470),
+              height: Screenadapter.width(130),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.question_answer, color: Colors.black54, size: Screenadapter.width(60)),
+                      Text("客服", style: TextStyle(color: Colors.black87, fontSize: Screenadapter.fontSize(32)),)
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(FangXMIcon.fuwu, color: Colors.black54, size: Screenadapter.width(60)),
+                      Text("收藏", style: TextStyle(color: Colors.black87, fontSize: Screenadapter.fontSize(32)),)
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.shopping_cart_checkout, color: Colors.black54, size: Screenadapter.width(60)),
+                      Text("购物车", style: TextStyle(color: Colors.black87, fontSize: Screenadapter.fontSize(32)),)
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Expanded(child: SizedBox(
+              height: Screenadapter.width(110),
+              child: ElevatedButton(
+                onPressed: (){
+
+                }, 
+                style: ButtonStyle(
+                  padding: WidgetStateProperty.all(EdgeInsets.zero),
+                  backgroundColor: WidgetStatePropertyAll(Colors.orange),
+                  foregroundColor: WidgetStatePropertyAll(Colors.white),
+                  // 按钮设置圆角
+                  shape: WidgetStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(Screenadapter.width(55)),
+                        bottomLeft: Radius.circular(Screenadapter.width(55)),
+                      ) 
+                    )
+                  )
+                ),
+                child: Text("加入购物车", style: TextStyle(fontSize: Screenadapter.fontSize(40)))
+              ),
+            )),
+            Expanded(child: Container(
+              height: Screenadapter.width(110),
+              child: ElevatedButton(
+                onPressed: (){
+
+                }, 
+                style: ButtonStyle(
+                  padding: WidgetStateProperty.all(EdgeInsets.zero),
+                  backgroundColor: WidgetStatePropertyAll(Color.fromARGB(235, 232, 59, 11)),
+                  foregroundColor: WidgetStatePropertyAll(Colors.white),
+                  shape: WidgetStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(Screenadapter.width(55)),
+                        bottomRight: Radius.circular(Screenadapter.width(55)),
+                      ) 
+                    )
+                  )
+                ),
+                child: Text("立即购买", style: TextStyle(fontSize: Screenadapter.fontSize(40)))
+              ),
+            )),
+          ],
+        ),
+      )
     );
   }
 
